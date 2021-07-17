47121f29c71aa067f6ad483427008f0152112dfa
ChoiEungi <choieungi@gm.gist.ac.kr>
2021-07-17 23:55:57 +0900

    init

---
+++ src/main/java/com/example/gistcompetitioncnserver/GistCompetitionCnServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gistcompetitioncnserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GistCompetitionCnServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GistCompetitionCnServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gistcompetitioncnserver/GistCompetitionCnServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gistcompetitioncnserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GistCompetitionCnServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
