ce9f55182b71c1dde69eb065ba19002549d74587
koseyeon <koseyeon97@gmail.com>
2021-09-09 14:01:41 +0900

    [feat] connect to S3 and create API for file upload to S3

---
+++ src/main/java/com/example/gistcompetitioncnserver/file/S3UploadService.java
@@ -0,0 +1,27 @@
+package com.example.gistcompetitioncnserver.file;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+
+@RequiredArgsConstructor
+@Component
+public class S3UploadService implements UploadService{
+    private final AmazonS3 amazonS3;
+    private final S3Component component;
+
+    @Override
+    public void uploadFile(InputStream inputStream, ObjectMetadata objectMetadata, String fileName) {
+        amazonS3.putObject(new PutObjectRequest(component.getBucket(), fileName, inputStream, objectMetadata).withCannedAcl(CannedAccessControlList.PublicRead));
+    }
+
+    @Override
+    public String getFileUrl(String fileName) {
+        return amazonS3.getUrl(component.getBucket(), fileName).toString();
+    }
+}
