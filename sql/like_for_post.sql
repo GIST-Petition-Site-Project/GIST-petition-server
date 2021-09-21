2f0ea14876e77f3289fe2e65a4d4d90d1a1a5f93
koseyeon <koseyeon97@gmail.com>
2021-09-21 16:00:59 +0900

    [feat] 좋아요 기능 구현

---
+++ sql/like_for_post.sql
@@ -0,0 +1,9 @@
+-- auto-generated definition
+create table like_for_post
+(
+    like_id bigint auto_increment
+        primary key,
+    user_id    bigint       null,
+    post_id    bigint       null,
+    foreign key (post_id) references post (post_id)
+);
