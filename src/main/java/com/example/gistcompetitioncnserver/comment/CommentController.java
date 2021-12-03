301e5781e2c65a49b82e1db7fbf695da73152f86
gurunelee <dlckdgk4858@gmail.com>
2021-12-03 15:32:35 +0900

    refactor(CommentController): createComment 리턴하는 자료형 ReponseEntity<Object>->ResponseEntity<Void>, pathVariable 이름 변경 id->postId

--- src/main/java/com/example/gistcompetitioncnserver/comment/CommentController.java
+++ src/main/java/com/example/gistcompetitioncnserver/comment/CommentController.java
@@ -19,53 +19,51 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @AllArgsConstructor
 @RestController
 @RequestMapping("/gistps/api/v1/post")
 public class CommentController {
 
-    private final CommentService commentService;
-    private final PostService postService;
-    private final UserService userService;
+  private final CommentService commentService;
+  private final PostService postService;
+  private final UserService userService;
 
-    @PostMapping("/{id}/comment")
-    public ResponseEntity<Object> createComment(@PathVariable Long id, @RequestBody CommentRequest
-            commentRequest, @AuthenticationPrincipal String email) {
-        User user = userService.findUserByEmail2(email);
+  @PostMapping("/{postId}/comment")
+  public ResponseEntity<Void> createComment(
+      @PathVariable Long postId,
+      @RequestBody CommentRequest commentRequest,
+      @AuthenticationPrincipal String email) {
+    User user = userService.findUserByEmail2(email);
 
-        Long commentId = commentService.createComment(id, commentRequest, user.getId());
-        return ResponseEntity
-                .created(URI.create("/post/" + id + "/comment/" + commentId))
-                .build();
+    Long commentId = commentService.createComment(postId, commentRequest, user.getId());
+    return ResponseEntity.created(URI.create("/post/" + postId + "/comment/" + commentId)).build();
+  }
+
+  @DeleteMapping("/{id}/comment/{commentId}")
+  public ResponseEntity<Object> deleteComment(
+      @PathVariable Long id, @PathVariable Long commentId, @AuthenticationPrincipal String email) {
+
+    User user = userService.findUserByEmail2(email);
+
+    Optional<Post> post = postService.retrievePost(id);
+    if (post.isEmpty()) {
+      throw new CustomException(ErrorCase.NO_SUCH_POST_ERROR);
     }
 
-    @DeleteMapping("/{id}/comment/{commentId}")
-    public ResponseEntity<Object> deleteComment(@PathVariable Long id, @PathVariable Long commentId,
-                                                @AuthenticationPrincipal String email) {
-
-        User user = userService.findUserByEmail2(email);
-
-        Optional<Post> post = postService.retrievePost(id);
-        if (post.isEmpty()) {
-            throw new CustomException(ErrorCase.NO_SUCH_POST_ERROR);
-        }
-
-        if (!commentService.existCommentId(commentId)) {
-            throw new CustomException(ErrorCase.NO_SUCH_COMMENT_ERROR);
-        }
-
-        if (!commentService.equalUserToComment(commentId, user.getId())) {
-            throw new CustomException(ErrorCase.FORBIDDEN_ERROR);
-        }
-
-        commentService.deleteComment(commentId);
-        return ResponseEntity.noContent().build();
+    if (!commentService.existCommentId(commentId)) {
+      throw new CustomException(ErrorCase.NO_SUCH_COMMENT_ERROR);
     }
 
-    @GetMapping("/{id}/comment")
-    public ResponseEntity<Object> getComments(@PathVariable Long id) {
-        return ResponseEntity.ok().body(commentService.getCommentsByPostId(id));
+    if (!commentService.equalUserToComment(commentId, user.getId())) {
+      throw new CustomException(ErrorCase.FORBIDDEN_ERROR);
     }
 
+    commentService.deleteComment(commentId);
+    return ResponseEntity.noContent().build();
+  }
+
+  @GetMapping("/{id}/comment")
+  public ResponseEntity<Object> getComments(@PathVariable Long id) {
+    return ResponseEntity.ok().body(commentService.getCommentsByPostId(id));
+  }
 }
