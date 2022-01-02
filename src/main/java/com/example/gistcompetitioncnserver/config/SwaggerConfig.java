5b3627fb297e8279fe54b237cb1337fd4b9175c7
Wannte <T.wannte@gmail.com>
2022-01-02 19:35:58 +0900

    chore: dummy-user(ADMIN권한) 추가
    추가로 swagger api 설정 조금 수정했습니다

--- src/main/java/com/example/gistcompetitioncnserver/config/SwaggerConfig.java
+++ src/main/java/com/example/gistcompetitioncnserver/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.example.gistcompetitioncnserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -17,7 +18,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.OAS_30) // open api spec 3.0
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build();
     }
