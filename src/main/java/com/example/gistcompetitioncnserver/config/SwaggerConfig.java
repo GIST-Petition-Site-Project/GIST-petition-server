4a00ec96170db039b6bde364114a1d6f2b932eb0
wannte <49307266+wannte@users.noreply.github.com>
2022-01-02 21:20:28 +0900

    chore: swagger host 설정 추가 및 api 경로 수정 (#86)
    * fix: swagger host 설정 추가
    
    * chore: profile 설정 수정

--- src/main/java/com/example/gistcompetitioncnserver/config/SwaggerConfig.java
+++ src/main/java/com/example/gistcompetitioncnserver/config/SwaggerConfig.java
@@ -1,8 +1,8 @@
 package com.example.gistcompetitioncnserver.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,15 +10,23 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Collections;
+
 @Configuration
 public class SwaggerConfig {
+    @Value("${swagger.host:localhost:8080}")
+    private String host;
+    @Value("${swagger.protocol:http}")
+    private String protocol;
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.OAS_30) // open api spec 3.0
+        return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                .host(host)
+                .protocols(Collections.singleton(protocol))
                 .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .apis(RequestHandlerSelectors.basePackage("com.example.gistcompetitioncnserver"))
                 .paths(PathSelectors.any())
                 .build();
     }
