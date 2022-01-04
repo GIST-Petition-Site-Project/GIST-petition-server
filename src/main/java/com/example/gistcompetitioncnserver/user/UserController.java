34d10bda7fa5243064ab4ee4556f530ac1937e84
koseyeon <koseyeon97@gmail.com>
2022-01-04 13:34:19 +0900

    feat: logout 컨트롤러 구현.

--- src/main/java/com/example/gistcompetitioncnserver/user/UserController.java
+++ src/main/java/com/example/gistcompetitioncnserver/user/UserController.java
@@ -2,9 +2,11 @@ package com.example.gistcompetitioncnserver.user;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.net.URI;
 import java.util.List;
 
@@ -14,7 +16,7 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
-
+    private final HttpSession httpSession;
     @PostMapping("/users")
     public ResponseEntity<Void> register(@Validated @RequestBody SignUpRequest request) {
         return ResponseEntity.created(URI.create("/users/" + userService.signUp(request))).build();
@@ -25,7 +27,11 @@ public class UserController {
         userService.signIn(request);
         return ResponseEntity.ok().build();
     }
-
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(){
+        httpSession.removeAttribute("user");
+        return ResponseEntity.ok().build();
+    }
 
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
