cce0fcb62905221b377a3b321dfc76c86915a6ad
wannte <T.wannte@gmail.com>
2022-01-07 23:51:59 +0900

    refactor(postService): post를 조회하고 사용하지 않는 부분 수정. 존재 유무만을 확인

--- src/main/java/com/example/gistcompetitioncnserver/post/PostService.java
+++ src/main/java/com/example/gistcompetitioncnserver/post/PostService.java
@@ -43,7 +43,7 @@ public class PostService {
 
     @Transactional(readOnly = true)
     public Post retrievePost(Long postId) {
-        return findPostById(postId);
+        return postRepository.findById(postId).orElseThrow(() -> new CustomException(ErrorCase.NO_SUCH_POST_ERROR));
     }
 
     @Transactional(readOnly = true)
@@ -58,43 +58,41 @@ public class PostService {
 
     @Transactional
     public void updatePostDescription(Long postId, String description) {
-        Post post = findPostById(postId);
+        Post post = postRepository.findById(postId).orElseThrow(() -> new CustomException(ErrorCase.NO_SUCH_POST_ERROR));
         post.setDescription(description);
     }
 
     @Transactional
     public void deletePost(Long postId) {
-        Post post = findPostById(postId);
-        commentRepository.deleteByPostId(post.getId());
-        postRepository.deleteById(post.getId());
+        if (postRepository.existsById(postId)) {
+            throw new CustomException(ErrorCase.NO_SUCH_POST_ERROR);
+        }
+        commentRepository.deleteByPostId(postId);
+        postRepository.deleteById(postId);
     }
 
 
     @Transactional
     public Boolean agree(Long postId, Long userId) {
-        Post post = findPostById(postId);
+        Post post = postRepository.findById(postId).orElseThrow(() -> new CustomException(ErrorCase.NO_SUCH_POST_ERROR));
         User user = findUserById(userId);
         return post.applyAgreement(user);
     }
 
     @Transactional(readOnly = true)
     public int getNumberOfAgreements(Long id) {
-        Post post = findPostById(id);
+        Post post = postRepository.findById(id).orElseThrow(() -> new CustomException(ErrorCase.NO_SUCH_POST_ERROR));
         return post.getAgreements().size();
     }
 
     @Transactional(readOnly = true)
     public Boolean getStateOfAgreement(Long postId, Long userId) {
-        Post post = findPostById(postId);
+        Post post = postRepository.findById(postId).orElseThrow(() -> new CustomException(ErrorCase.NO_SUCH_POST_ERROR));
         User user = findUserById(userId);
         return post.isAgreedBy(user);
     }
 
     private User findUserById(Long userId) {
         return userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCase.NO_SUCH_USER_ERROR));
-    }
-
-    private Post findPostById(Long postId) {
-        return postRepository.findById(postId).orElseThrow(() -> new CustomException(ErrorCase.NO_SUCH_POST_ERROR));
     }
 }
