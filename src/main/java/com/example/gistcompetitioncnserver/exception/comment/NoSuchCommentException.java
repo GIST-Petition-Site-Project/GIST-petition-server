266b881b04b8b3c91dacdd8779340fa259b65817
gurunelee <dlckdgk4858@gmail.com>
2022-01-08 19:59:16 +0900

    feat: exception/comment 를 정의한다

---
+++ src/main/java/com/example/gistcompetitioncnserver/exception/comment/NoSuchCommentException.java
@@ -0,0 +1,11 @@
+package com.example.gistcompetitioncnserver.exception.comment;
+
+import org.springframework.http.HttpStatus;
+
+public class NoSuchCommentException extends CommentException {
+    private static final String MESSAGE = "존재하지 않는 댓글입니다.";
+    private static final HttpStatus HTTP_STATUS = null;
+    public NoSuchCommentException() {
+        super(MESSAGE, HTTP_STATUS);
+    }
+}
