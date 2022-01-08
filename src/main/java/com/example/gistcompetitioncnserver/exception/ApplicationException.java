158257ca2b936b2623f4b9b6ae81918d5ad98323
gurunelee <dlckdgk4858@gmail.com>
2022-01-08 19:58:07 +0900

    feat: ApplicationException 을 정의한다

---
+++ src/main/java/com/example/gistcompetitioncnserver/exception/ApplicationException.java
@@ -0,0 +1,16 @@
+package com.example.gistcompetitioncnserver.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ApplicationException extends RuntimeException{
+    private final HttpStatus httpStatus;
+
+    public ApplicationException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+}
