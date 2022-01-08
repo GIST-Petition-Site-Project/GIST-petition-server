1e4a3501a7640989462127528471232e0d520352
gurunelee <dlckdgk4858@gmail.com>
2022-01-08 21:21:11 +0900

    refactor: CustomException 수정
    CustomException 이 CheckedException 과 메시지, httpStatus 를 담을 수 있도록 수정. 기본 HttpStatus 는 400 (Bad Request)

--- src/main/java/com/example/gistcompetitioncnserver/exception/CustomException.java
+++ src/main/java/com/example/gistcompetitioncnserver/exception/CustomException.java
@@ -1,7 +1,17 @@
 package com.example.gistcompetitioncnserver.exception;
 
+import org.springframework.http.HttpStatus;
+
 public class CustomException extends RuntimeException{
-    public CustomException(String message) {
+    public final HttpStatus httpStatus;
+    public final Throwable cause;
+
+    public CustomException(String message, Throwable cause) {
+        this(message, cause, HttpStatus.BAD_REQUEST);
+    }
+    public CustomException(String message, Throwable cause, HttpStatus httpStatus) {
         super(message);
+        this.cause = cause;
+        this.httpStatus = httpStatus;
     }
 }
