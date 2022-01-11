23b99e2acb2af2f4f0a48553e60c9aacd4c78f25
wannte <T.wannte@gmail.com>
2022-01-11 22:42:06 +0900

    chore: cors origin 지정

--- src/main/java/com/example/gistcompetitioncnserver/config/WebConfig.java
+++ src/main/java/com/example/gistcompetitioncnserver/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("localhost:3000", "petition-bucket.s3-website.ap-northeast-2.amazonaws.com")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
