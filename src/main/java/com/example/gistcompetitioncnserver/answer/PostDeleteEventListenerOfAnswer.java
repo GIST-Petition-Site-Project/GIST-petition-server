44f9085177e2ef04d70708fc4e7839440e8117f3
wannte <49307266+wannte@users.noreply.github.com>
2022-01-11 18:38:05 +0900

    refactor: Post 삭제 시 이벤트 방식으로 댓글, 답변 삭제 (#117)
    * refactor: Post 삭제 시 댓글, 답변 삭제. findByPostId() 메소드를 사용하도록 리팩토링
    
    * refactor: 모호한 메소드 명 수정 PageNumber -> PostCount

---
+++ src/main/java/com/example/gistcompetitioncnserver/answer/PostDeleteEventListenerOfAnswer.java
@@ -0,0 +1,19 @@
+package com.example.gistcompetitioncnserver.answer;
+
+import com.example.gistcompetitioncnserver.post.PostDeleteEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostDeleteEventListenerOfAnswer implements ApplicationListener<PostDeleteEvent> {
+    private final AnswerRepository answerRepository;
+
+    public PostDeleteEventListenerOfAnswer(AnswerRepository answerRepository) {
+        this.answerRepository = answerRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(PostDeleteEvent event) {
+        answerRepository.deleteByPostId(event.getPostId());
+    }
+}
