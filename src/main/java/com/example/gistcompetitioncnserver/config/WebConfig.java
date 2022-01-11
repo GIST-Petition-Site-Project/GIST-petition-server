d71a9d7323a35ecc07a2a7e39014a6e335575b8c
wannte <T.wannte@gmail.com>
2022-01-11 20:46:51 +0900

    feat: interceptor, argumentResolver 등록

--- src/main/java/com/example/gistcompetitioncnserver/config/WebConfig.java
+++ src/main/java/com/example/gistcompetitioncnserver/config/WebConfig.java
@@ -1,11 +1,22 @@
 package com.example.gistcompetitioncnserver.config;
 
+import com.example.gistcompetitioncnserver.user.LoginInterceptor;
+import com.example.gistcompetitioncnserver.user.LoginUserArgumentResolver;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@AllArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final LoginInterceptor loginInterceptor;
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -15,5 +26,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
     }
 }
