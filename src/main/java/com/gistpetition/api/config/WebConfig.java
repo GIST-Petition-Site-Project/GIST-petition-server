b868fa6b7f6dec200344e3d465cb0111b5661fb9
wannte <T.wannte@gmail.com>
2022-01-15 01:46:39 +0900

    feat: api 로깅 추가

--- src/main/java/com/gistpetition/api/config/WebConfig.java
+++ src/main/java/com/gistpetition/api/config/WebConfig.java
@@ -1,11 +1,14 @@
 package com.gistpetition.api.config;
 
+import ch.qos.logback.access.servlet.TeeFilter;
 import com.gistpetition.api.config.argumentresolver.LoginUserArgumentResolver;
 import com.gistpetition.api.config.interceptor.AdminPermissionInterceptor;
 import com.gistpetition.api.config.interceptor.LoginInterceptor;
 import com.gistpetition.api.config.interceptor.ManagerPermissionInterceptor;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -17,7 +20,7 @@ import java.util.List;
 @Configuration
 @AllArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-    @Value("${request.origins:*}")
+    @Value("${request.origins:https://localhost:8080}")
     String[] origins;
 
     private final LoginInterceptor loginInterceptor;
@@ -44,5 +47,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserArgumentResolver);
+    }
+
+    @Bean
+    public FilterRegistrationBean requestLoggingFilter() {
+        return new FilterRegistrationBean(new TeeFilter());
     }
 }
