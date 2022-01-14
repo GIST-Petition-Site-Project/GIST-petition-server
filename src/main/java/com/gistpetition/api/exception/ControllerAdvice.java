7709da083b7fe46c1a6faf8a9e0b23d620056bf3
wannte <49307266+wannte@users.noreply.github.com>
2022-01-14 22:57:29 +0900

    feat: Loggin 전략 도입 (#147)
    * feat: ControllerAdvice 로깅 추가
    
    * feat: logback 설정 추가
    
    * chore: 자동으로 설정되는 jpa.database-platform 삭제
    
    https://docs.spring.io/spring-boot/docs/current/reference/html/application-properties.html 참고
    
    * chore: docker-compose submodule 반영
    
    * refactor: 무의미한 엔터 제거
    
    Co-authored-by: ChangHa Lee <60911416+GuruneLee@users.noreply.github.com>

--- src/main/java/com/gistpetition/api/exception/ControllerAdvice.java
+++ src/main/java/com/gistpetition/api/exception/ControllerAdvice.java
@@ -1,5 +1,7 @@
 package com.gistpetition.api.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,20 +9,30 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerAdvice {
+    private final static Logger LOGGER = LoggerFactory.getLogger(ControllerAdvice.class);
 
     @ExceptionHandler(ApplicationException.class)
     public ResponseEntity<ErrorResponse> handle(ApplicationException ex) {
+        LOGGER.info(String.format("ApplicationException: %s", ex.getMessage()));
         return ResponseEntity.status(ex.getHttpStatus()).body(new ErrorResponse(ex.getMessage()));
     }
 
     @ExceptionHandler(WrappedException.class)
     public ResponseEntity<ErrorResponse> handle(WrappedException ex) {
+        LOGGER.info(String.format("WrappedException: %s", ex.getCause().getMessage()));
         return ResponseEntity.status(ex.getHttpStatus()).body(new ErrorResponse(ex.getCause().getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> validException(MethodArgumentNotValidException ex) {
         String message = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        LOGGER.info(String.format("MethodArgumentNotValidException: %s", message));
         return ResponseEntity.badRequest().body(new ErrorResponse(message));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> validException(Exception ex) {
+        LOGGER.error("InternalServerError: ", ex);
+        return ResponseEntity.internalServerError().body(new ErrorResponse("Internal Server Error"));
     }
 }
