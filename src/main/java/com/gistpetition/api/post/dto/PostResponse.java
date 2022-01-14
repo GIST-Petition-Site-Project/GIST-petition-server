8d863b62c6e61b36c0e902656b047bf30c441191
koseyeon <koseyeon97@gmail.com>
2022-01-15 00:09:36 +0900

    fix: PostResponse에 시간 추가

--- src/main/java/com/gistpetition/api/post/dto/PostResponse.java
+++ src/main/java/com/gistpetition/api/post/dto/PostResponse.java
@@ -1,25 +1,23 @@
 package com.gistpetition.api.post.dto;
 
 import com.gistpetition.api.post.domain.Post;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Getter
+@RequiredArgsConstructor
 public class PostResponse {
     private final String title;
     private final String description;
     private final String categoryName;
-
-    public PostResponse(String title, String description, String categoryName) {
-        this.title = title;
-        this.description = description;
-        this.categoryName = categoryName;
-    }
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
 
     public static Page<PostResponse> pageOf(Page<Post> page) {
         List<PostResponse> postResponseList = page.getContent().stream().map(PostResponse::of).collect(Collectors.toList());
@@ -27,19 +25,12 @@ public class PostResponse {
     }
 
     public static PostResponse of(Post post) {
-        return new PostResponse(post.getTitle(), post.getDescription(), post.getCategory().getName());
+        return new PostResponse(
+                post.getTitle(),
+                post.getDescription(),
+                post.getCategory().getName(),
+                post.getCreatedAt(),
+                post.getUpdatedAt()
+        );
     }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
 }
