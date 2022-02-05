c9cc4dbad0f0cba6066729875768376787592360
grace_goose <choieungi@gm.gist.ac.kr>
2022-02-05 19:16:26 +0900

    refactor: SignUpValidator package를 user에서 verification으로 이동

--- src/main/java/com/gistpetition/api/user/application/UserService.java
+++ src/main/java/com/gistpetition/api/user/application/UserService.java
@@ -14,6 +14,7 @@ import com.gistpetition.api.user.dto.request.UpdateUserRoleRequest;
 import com.gistpetition.api.utils.email.EmailDomain;
 import com.gistpetition.api.utils.email.EmailParser;
 import com.gistpetition.api.utils.password.Encoder;
+import com.gistpetition.api.verification.application.SignUpValidator;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
