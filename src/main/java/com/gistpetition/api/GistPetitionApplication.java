dd5e4e86eaa96376b18e219cc7df1f9f96476394
wannte <T.wannte@gmail.com>
2022-02-10 17:28:05 +0900

    refactor: JpaConfiguration 분리
    패키지 위치로 인해, basePackage를 지정하지 않게 되면 Bean등록에 문제가 발생해서 별도로 지정해줬습니다

--- src/main/java/com/gistpetition/api/GistPetitionApplication.java
+++ src/main/java/com/gistpetition/api/GistPetitionApplication.java
@@ -2,12 +2,8 @@ package com.gistpetition.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.envers.repository.config.EnableEnversRepositories;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
-@EnableEnversRepositories
 public class GistPetitionApplication {
 
     public static void main(String[] args) {
