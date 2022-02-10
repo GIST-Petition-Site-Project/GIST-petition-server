c3d09257784e92f4c9f8cfec15f0fcdbc027eee5
wannte <T.wannte@gmail.com>
2022-02-10 17:54:39 +0900

    chore: DataLoader 에서 청원 등록 로직 삭제

--- src/main/java/com/gistpetition/api/DataLoader.java
+++ src/main/java/com/gistpetition/api/DataLoader.java
@@ -1,8 +1,5 @@
 package com.gistpetition.api;
 
-import com.gistpetition.api.petition.domain.Category;
-import com.gistpetition.api.petition.domain.Petition;
-import com.gistpetition.api.petition.domain.PetitionRepository;
 import com.gistpetition.api.user.domain.User;
 import com.gistpetition.api.user.domain.UserRepository;
 import com.gistpetition.api.user.domain.UserRole;
@@ -12,32 +9,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Profile("dev")
 @RequiredArgsConstructor
 @Component
 public class DataLoader implements CommandLineRunner {
     private final User ADMIN = new User(1L, "admin@gist.ac.kr", new BcryptEncoder().hashPassword("test1234!"), UserRole.ADMIN);
     private final UserRepository userRepository;
-    private final PetitionRepository petitionRepository;
 
     @Override
     public void run(String... args) {
         userRepository.save(ADMIN);
-
-        List<Petition> petitionsAboutDorm = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            petitionsAboutDorm.add(new Petition("dorm " + i, "description" + i, Category.DORMITORY, 1L));
-        }
-
-        List<Petition> petitionsAboutFacility = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            petitionsAboutFacility.add(new Petition("professor " + i, "description" + i, Category.FACILITY, 1L));
-        }
-
-        petitionRepository.saveAll(petitionsAboutDorm);
-        petitionRepository.saveAll(petitionsAboutFacility);
     }
 }
