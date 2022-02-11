69e10e3c2bdc06d9313c525ae92764c88aa68d96
wannte <T.wannte@gmail.com>
2022-02-11 22:42:45 +0900

    feat: Timezone `Asia/Seoul` default 설정
    script에서 진행하던 부분을 Spring의 `@PostConstruct`를 통해 진행.

--- src/main/java/com/gistpetition/api/GistPetitionApplication.java
+++ src/main/java/com/gistpetition/api/GistPetitionApplication.java
@@ -1,10 +1,22 @@
 package com.gistpetition.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.time.ZonedDateTime;
+import java.util.TimeZone;
+
+@Slf4j
 @SpringBootApplication
 public class GistPetitionApplication {
+
+    @PostConstruct
+    public void initTimezone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        log.info("current Timezone : {}, current DateTime : {}", TimeZone.getDefault().getDisplayName(), ZonedDateTime.now());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GistPetitionApplication.class, args);
