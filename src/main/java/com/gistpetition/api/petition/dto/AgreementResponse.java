7f0533d93e1dd12efdad851e7397bf02a0bfb699
wannte <T.wannte@gmail.com>
2022-02-12 14:25:20 +0900

    refactor: AgreementResponse 구조 수정

--- src/main/java/com/gistpetition/api/petition/dto/AgreementResponse.java
+++ src/main/java/com/gistpetition/api/petition/dto/AgreementResponse.java
@@ -1,27 +1,17 @@
 package com.gistpetition.api.petition.dto;
 
 import com.gistpetition.api.petition.domain.Agreement;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.Data;
 import org.springframework.data.domain.Page;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Getter
-@RequiredArgsConstructor
+@Data
 public class AgreementResponse {
 
-    String description;
-    Long agreementId;
-
-    public AgreementResponse(String description, Long agreementId){
-        this.description = description;
-        this.agreementId = agreementId;
-    }
+    private final Long id;
+    private final String description;
 
     public static AgreementResponse of(Agreement agreement) {
-        return new AgreementResponse(agreement.getDescription(), agreement.getId());
+        return new AgreementResponse(agreement.getId(), agreement.getDescription());
     }
 
     public static Page<AgreementResponse> pageOf(Page<Agreement> agreements) {
