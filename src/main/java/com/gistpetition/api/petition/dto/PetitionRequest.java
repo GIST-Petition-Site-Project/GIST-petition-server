587730ce89322b55f7d940f7d55fe7ccf6ad1b33
gurunelee <dlckdgk4858@gmail.com>
2022-02-14 16:54:22 +0900

    refactor: @NotBlank 는 CharacterSequence 에만 적용할 수 있다.

--- src/main/java/com/gistpetition/api/petition/dto/PetitionRequest.java
+++ src/main/java/com/gistpetition/api/petition/dto/PetitionRequest.java
@@ -8,7 +8,7 @@ public class PetitionRequest {
     private String title;
     @NotBlank
     private String description;
-    @NotBlank
+    @NotNull
     private Long categoryId;
 
     protected PetitionRequest() {
