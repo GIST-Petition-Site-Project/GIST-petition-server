58fd8995f8801d0b1a9bcbcc0bce3dfb0fa113e2
wannte <T.wannte@gmail.com>
2022-02-15 21:20:55 +0900

    chore: DataLoader 값 추가

--- src/main/java/com/gistpetition/api/DataLoader.java
+++ src/main/java/com/gistpetition/api/DataLoader.java
@@ -17,6 +17,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -48,24 +49,23 @@ public class DataLoader implements CommandLineRunner {
 
         userRepository.save(new User("admin@gist.ac.kr", PASSWORD, UserRole.ADMIN));
         userRepository.save(new User("manager@gist.ac.kr", PASSWORD, UserRole.MANAGER));
+        User user = userRepository.save(new User("user@gist.ac.kr", PASSWORD, UserRole.USER));
 
-        User a = userRepository.save(new User("a@gist.ac.kr", PASSWORD, UserRole.USER));
-        User b = userRepository.save(new User("b@gist.ac.kr", PASSWORD, UserRole.USER));
-        User c = userRepository.save(new User("c@gist.ac.kr", PASSWORD, UserRole.USER));
-        User d = userRepository.save(new User("d@gist.ac.kr", PASSWORD, UserRole.USER));
-        User e = userRepository.save(new User("e@gist.ac.kr", PASSWORD, UserRole.USER));
-        List<User> users = Arrays.asList(a, b, c, d, e);
+        ArrayList<User> alphabetUsers = new ArrayList<>();
+        for (char alphabet = 'a'; alphabet <= 'z'; alphabet++) {
+            alphabetUsers.add(userRepository.save(new User(alphabet + "@gist.ac.kr", PASSWORD, UserRole.USER)));
+        }
 
-        Petition petition1 = petitionRepository.save(new Petition("국민 청원에 글을 써버렸다.", CONTENT, Category.DORMITORY, a.getId()));
-        Petition petition2 = petitionRepository.save(new Petition("방송 촬영을 위해 안전과 생존을 위협당하는 동물의 대책 마련이 필요합니다", CONTENT, Category.DORMITORY, a.getId()));
-        Petition petition3 = petitionRepository.save(new Petition("길고양이를 학대하는 갤러리를 폐쇄하고 엄중한 수사를 해주십시오.", CONTENT, Category.DORMITORY, a.getId()));
-        Petition petition4 = petitionRepository.save(new Petition("코로나19로 부터 우리 아이들을 지켜주세요.", CONTENT, Category.DORMITORY, a.getId()));
-        Petition petition5 = petitionRepository.save(new Petition("2차방역지원금 지금 자영업분들 농락하시나요?", CONTENT, Category.DORMITORY, a.getId()));
+        Petition petition1 = petitionRepository.save(new Petition("국민 청원에 글을 써버렸다.", CONTENT, Category.DORMITORY, user.getId()));
+        Petition petition2 = petitionRepository.save(new Petition("방송 촬영을 위해 안전과 생존을 위협당하는 동물의 대책 마련이 필요합니다", CONTENT, Category.DORMITORY, user.getId()));
+        Petition petition3 = petitionRepository.save(new Petition("길고양이를 학대하는 갤러리를 폐쇄하고 엄중한 수사를 해주십시오.", CONTENT, Category.DORMITORY, user.getId()));
+        Petition petition4 = petitionRepository.save(new Petition("코로나19로 부터 우리 아이들을 지켜주세요.", CONTENT, Category.DORMITORY, user.getId()));
+        Petition petition5 = petitionRepository.save(new Petition("2차방역지원금 지금 자영업분들 농락하시나요?", CONTENT, Category.DORMITORY, user.getId()));
         List<Petition> petitions = Arrays.asList(petition1, petition2, petition3, petition4, petition5);
 
         for (Petition petition : petitions) {
-            for (User user : users) {
-                petitionService.agree(AGREEMENT_REQUEST, petition.getId(), user.getId());
+            for (User alphabetUser : alphabetUsers) {
+                petitionService.agree(AGREEMENT_REQUEST, petition.getId(), alphabetUser.getId());
             }
         }
     }
