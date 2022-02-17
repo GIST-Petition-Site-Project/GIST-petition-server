602ce7a13a186491a784ab86cd7f2412e5c9433e
koseyeon <koseyeon97@gmail.com>
2022-02-17 16:39:51 +0900

    feat: concurrency user test 구현

--- src/main/java/com/gistpetition/api/user/domain/User.java
+++ src/main/java/com/gistpetition/api/user/domain/User.java
@@ -9,6 +9,7 @@ public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String username;
     private String password;
     @Enumerated(EnumType.STRING)
