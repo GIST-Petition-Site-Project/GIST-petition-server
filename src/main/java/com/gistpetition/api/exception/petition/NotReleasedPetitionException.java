6abde29a2a1af8eb7b8b05aadd9ea07995993844
wannte <T.wannte@gmail.com>
2022-02-19 15:34:44 +0900

    refactor: 공개되지 않은 청원 접근시 FORBIDDEN 상태코드 반환

--- src/main/java/com/gistpetition/api/exception/petition/NotReleasedPetitionException.java
+++ src/main/java/com/gistpetition/api/exception/petition/NotReleasedPetitionException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public class NotReleasedPetitionException extends PetitionException {
     private static final String MESSAGE = "공개되지 않은 청원 게시물입니다.";
-    private static final HttpStatus HTTP_STATUS = HttpStatus.BAD_REQUEST;
+    private static final HttpStatus HTTP_STATUS = HttpStatus.FORBIDDEN;
 
     public NotReleasedPetitionException() {
         super(MESSAGE, HTTP_STATUS);
