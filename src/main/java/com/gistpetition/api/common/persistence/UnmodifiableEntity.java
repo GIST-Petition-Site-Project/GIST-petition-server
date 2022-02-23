567f53277e8ac5718ea42b791bb70c22fc452901
ChangHa Lee <60911416+GuruneLee@users.noreply.github.com>
2022-02-23 14:35:39 +0900

    feat: 도메인과 DB 레이어를 분리한다. (#242)
    * refactor: id를 BaseEntity 와 UnmodifiableEntity 로 이동.
    
    * feat: 만료된 청원에 동의 실패하는 테스트 추가.
    
    * feat: add agree 메서드 시간 의존성 제거. Service layer 로 올림.
    
    * feat: release 메서드 시간 의존성 제거. Service layer 로 올림.

--- src/main/java/com/gistpetition/api/common/persistence/UnmodifiableEntity.java
+++ src/main/java/com/gistpetition/api/common/persistence/UnmodifiableEntity.java
@@ -3,17 +3,24 @@ package com.gistpetition.api.common.persistence;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class UnmodifiableEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @CreatedDate
     private LocalDateTime createdAt;
 
     protected UnmodifiableEntity() {
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public LocalDateTime getCreatedAt() {
--- src/main/java/com/gistpetition/api/petition/application/PetitionService.java
+++ src/main/java/com/gistpetition/api/petition/application/PetitionService.java
@@ -41,6 +41,7 @@ public class PetitionService {
                         petitionRequest.getTitle(),
                         petitionRequest.getDescription(),
                         Category.of(petitionRequest.getCategoryId()),
+                        LocalDateTime.now().plusDays(POSTING_PERIOD),
                         userId,
                         tempUrl));
         return created.getId();
@@ -154,7 +155,7 @@ public class PetitionService {
         Petition petition = findPetitionById(petitionId);
         User user = findUserById(userId);
         Agreement agreement = new Agreement(request.getDescription(), user.getId());
-        agreement.setPetition(petition);
+        agreement.setPetition(petition, LocalDateTime.now());
         try {
             agreementRepository.save(agreement);
         } catch (DataIntegrityViolationException e) {
@@ -184,7 +185,7 @@ public class PetitionService {
     @Transactional
     public void releasePetition(Long petitionId) {
         Petition petition = findPetitionById(petitionId);
-        petition.release();
+        petition.release(LocalDateTime.now());
     }
 
     @Transactional(readOnly = true)
