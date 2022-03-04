11844e6f483504c87be17a56a58d0ec605e99f6d
Seyeon Ko <82452337+koseyeon@users.noreply.github.com>
2022-03-04 21:25:32 +0900

    chore: NotMatchedPasswordException의 메시지를 보다 명확히 함. (#274)
    (cherry picked from commit 63db9e1a42614849a6300c77deda923701cbb008)

--- src/main/java/com/gistpetition/api/exception/user/NotMatchedPasswordException.java
+++ src/main/java/com/gistpetition/api/exception/user/NotMatchedPasswordException.java
@@ -3,7 +3,7 @@ package com.gistpetition.api.exception.user;
 import org.springframework.http.HttpStatus;
 
 public class NotMatchedPasswordException extends UserException {
-    private static final String MESSAGE = "비밀번호가 맞지 않습니다.";
+    private static final String MESSAGE = "현재 비밀번호가 일치하지 않습니다.";
     private static final HttpStatus HTTP_STATUS = HttpStatus.BAD_REQUEST;
 
     public NotMatchedPasswordException() {
