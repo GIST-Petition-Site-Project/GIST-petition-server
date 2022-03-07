f7f37e9f20243af03b8f7f35ee703b2ef2593807
ChangHa Lee <60911416+GuruneLee@users.noreply.github.com>
2022-03-07 21:19:13 +0900

    feat: petition 엔터티의 agreements 를 일급객체로 분리하였다. (#280)
    * feat: List<Agreement> 의 일급객체로서 Agreements 를 정의하고 test를 작성하였다.
    
    * feat: petition 에서 동의에 관한 로직은 Agreements 로 포함시켰으므로, petition test 에서 petition 의 역할을 넘은 검증을 제거하였다. 이에 fetch join 쿼리가 필요가 없어졌다.
    
    * feat: Agreements 생성자를 protected 로 선언
    
    * refactor: Agreements 네이밍 변경 및 테스트 단순화 (의도한 부분이 잘 보이도록)
    
    * refactor: Agreements 네이밍 변경 및 테스트 단순화 (의도한 부분이 잘 보이도록)
    
    (cherry picked from commit dd3363257995a882453d58677941224cc7809e8e)

---
+++ src/test/java/com/gistpetition/api/agreement/AgreementsSupport.java
@@ -0,0 +1,9 @@
+package com.gistpetition.api.agreement;
+
+import com.gistpetition.api.petition.domain.Agreements;
+
+public class AgreementsSupport extends Agreements {
+    public AgreementsSupport() {
+        super();
+    }
+}
