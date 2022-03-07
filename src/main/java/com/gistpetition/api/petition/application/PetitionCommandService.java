f85b99486f0a337ea35ac3584a54a1756c113948
wannte <T.wannte@gmail.com>
2022-03-08 02:02:37 +0900

    refactor: petitionService agree 수정
    flush되는 시점에 unique 에러가 발생하는데, 기존의 DataIntegrityHandler의 경우에는 Order 설정을 진행하지 않았었고 이는 Lowest_Precedence인 트랜잭션보다도 안쪽에서 시작되는 것으로 보인다.
    
    특별한 의미는 없지만 Order(1) 옵션을 추가해줌으로써 이를 해결해줄 수 있었다.
    
    DataIntegrityAspect -> T.begin() -> PetitionCommandService.agree() -> T.flush() -> T.commit() -> DataIntegrityAspect

--- src/main/java/com/gistpetition/api/petition/application/PetitionCommandService.java
+++ src/main/java/com/gistpetition/api/petition/application/PetitionCommandService.java
@@ -4,7 +4,10 @@ import com.gistpetition.api.config.annotation.DataIntegrityHandler;
 import com.gistpetition.api.exception.petition.DuplicatedAgreementException;
 import com.gistpetition.api.exception.petition.NoSuchPetitionException;
 import com.gistpetition.api.exception.user.NoSuchUserException;
-import com.gistpetition.api.petition.domain.*;
+import com.gistpetition.api.petition.domain.AgreementRepository;
+import com.gistpetition.api.petition.domain.Category;
+import com.gistpetition.api.petition.domain.Petition;
+import com.gistpetition.api.petition.domain.PetitionRepository;
 import com.gistpetition.api.petition.dto.AgreementRequest;
 import com.gistpetition.api.petition.dto.PetitionRequest;
 import com.gistpetition.api.user.domain.User;
@@ -66,9 +69,7 @@ public class PetitionCommandService {
     public void agree(AgreementRequest request, Long petitionId, Long userId) {
         Petition petition = findPetitionById(petitionId);
         User user = findUserById(userId);
-        Agreement agreement = new Agreement(request.getDescription(), user.getId());
-        agreement.setPetition(petition, Instant.now());
-        agreementRepository.save(agreement);
+        petition.agree(user.getId(), request.getDescription(), Instant.now());
     }
 
     @Transactional
