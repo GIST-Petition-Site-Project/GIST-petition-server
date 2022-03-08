ab1b70b69d63ae36ab14426de5a9a18defe8cdd2
wannte <T.wannte@gmail.com>
2022-03-08 21:36:22 +0900

    refactor: AgreementsTest 패키지 위치 수정 및 가다듬기

--- src/test/java/com/gistpetition/api/agreement/AgreementsSupport.java
+++
@@ -1,9 +0,0 @@
-package com.gistpetition.api.agreement;
-
-import com.gistpetition.api.petition.domain.Agreements;
-
-public class AgreementsSupport extends Agreements {
-    public AgreementsSupport() {
-        super();
-    }
-}
