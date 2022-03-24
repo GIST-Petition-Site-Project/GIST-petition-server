ed3e419a79f42c6db9264c9e4f17ed872ac24207
gurunelee <dlckdgk4858@gmail.com>
2022-03-24 17:29:06 +0900

    chore: 고세연 사발면아

--- src/main/java/com/gistpetition/api/exception/petition/InvalidDescriptionLengthException.java
+++ src/main/java/com/gistpetition/api/exception/petition/InvalidDescriptionLengthException.java
@@ -2,7 +2,7 @@ package com.gistpetition.api.exception.petition;
 
 import org.springframework.http.HttpStatus;
 
-public class InvalidDescriptionLengthException extends PetitionException{
+public class InvalidDescriptionLengthException extends PetitionException {
     private static final String MESSAGE = "내용 길이가 올바르지 않습니다.";
     private static final HttpStatus HTTP_STATUS = HttpStatus.BAD_REQUEST;
 
